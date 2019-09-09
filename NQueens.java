872a241ee1715d4a7d36fd3c1744bc2683b4b84e
SerenaPang <ypang4@usfca.edu>
2019-09-08 18:25:41 -0700

    assignment02 done

--- NQueens.java
+++ NQueens.java
@@ -1,63 +1,90 @@
 
 public class NQueens {
-	
-	
- final int size;
- int board[][];
- int row ;
- int col;
 
-	NQueens(int n){
-	 size = n; 
-	 row = n;
-	 col = n;
+	final int size;
+	//int board[][];
+	 int board[][] = { { 0, 0, 0, 0 }, 
+             { 0, 0, 0, 0 }, 
+             { 0, 0, 0, 0 }, 
+             { 0, 0, 0, 0 } }; 
+	int row;
+	int col;
 
+	NQueens(int n) throws ValidationException {
+	
+
+		size = n;
+		row = n;
+		col = n;
+	}
+
+	boolean isValid() {
+		int i, j;
+
+		for (i = 0; i < col; i++)
+			if (board[row][i] == 1)
+				return false;
+
+		for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+			if (board[i][j] == 1)
+				return false;
+
+		for (i = row, j = col; j >= 0 && i < size; i++, j--)
+			if (board[i][j] == 1)
+				return false;
+
+		return true;
+	}
+
+	boolean placeNQueens() throws ValidationException {
+
+		if (size <= 0 || size ==2 || size ==3) {
+			throw new ValidationException("The size is not valid.");
+		}
+
+		if (col >= size)
+			return true;
+
+		for (int i = 0; i < size; i++) {
+
+			if (isValid()) {
+
+				board[i][col] = 1;
+
+				
+				if (placeNQueens() == true)
+					return true;
+
+				
+				board[i][col] = 0; 
+			}
+		}
+
+	
+		return false;
+	}
+
+	public void printToConsole() {
+
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++)
+				System.out.print(" " + board[i][j] + " ");
+			System.out.println();
+		}
 	}
 	
-	/*  Ensures the instance has a board of nxn, with n defined in the
-constructor. Attempts to place n queens on the nxn board so that no two
-queens attack each other. Returns true when successful, false otherwise.
-Throws Exception if “n” is smaller than 0.*/
 	
-	
-	
-//	 boolean placeNQueens() 
-//	    { 
-//	        int i, j; 
-//	  
-//	        for (i = 0; i < col; i++) 
-//	            if (board[row][i] == 1) 
-//	                return false; 
-//	  
-//	      
-//	        for (i = row, j = col; i >= 0 && j >= 0; i--, j--) 
-//	            if (board[i][j] == 1) 
-//	                return false; 
-//	  
-//	       
-//	        for (i = row, j = col; j >= 0 && i < size; i++, j--) 
-//	            if (board[i][j] == 1) 
-//	                return false; 
-//	  
-//	        return true; 
-//	    } 
-//	
-	
-
-	 boolean placeNQueens() 
-	 {
-	           
+	 boolean checkNQ() throws ValidationException 
+	    { 
+	       
+	  
+	        if (placeNQueens() == false) { 
+	            System.out.print("Solution does not exist"); 
+	            return false; 
+	        } 
+	  
+	        printToConsole(); 
 	        return true; 
 	    } 
-	
-public void printToConsole() {
-	
-	 for (int i = 0; i < size; i++) { 
-	        for (int j = 0; j < size; j++) 
-	            System.out.print(" " + board[i][j] 
-	                             + " "); 
-	        System.out.println(); 
-	    } 
-}
 
 }
